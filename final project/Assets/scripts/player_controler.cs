e7d22fa9b1825531057c07f768888ae9e966fc5f
uhumipa <dragonmonth666@gmail.com>
2025-03-20 15:59:38 +0800

    畫一點測試用地圖+角色移動代碼

---
+++ final project/Assets/scripts/player_controler.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class player_controler : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+    private void Move()
+    {
+        float movehorizontal;
+        float movevetical;
+        movehorizontal = Input.GetAxis("Horizontal");
+        movevetical = Input.GetAxis("Vertical");
+        if (movehorizontal != 0||movevetical!=0)
+        {
+            rb.linearVelocity = new Vector2(movehorizontal * speed, movevetical * speed);
+        }
+    }
+}
