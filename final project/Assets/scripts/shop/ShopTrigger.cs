893dc8ba296ba5c4eb2d458af8f36569fe9aba9c
Lin Chajie <jessy04282006@gmail.com>
2025-04-30 09:35:09 +0800

    農場切換到商店
    靠近商店時，按下Enter可以切換到商店場景。
    新增腳本: Assets>scripts>shop
    新增農場場景裡四張圖片在右上方草地

---
+++ final project/Assets/scripts/shop/ShopTrigger.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ShopTrigger : MonoBehaviour
+{
+    private bool playerInRange = false;
+
+    void Update()
+    {
+        if (playerInRange && Input.GetKeyDown(KeyCode.Return)) // Enter鍵
+        {
+            SceneManager.LoadScene("shop"); // 替換成你的商店場景名稱
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+}
