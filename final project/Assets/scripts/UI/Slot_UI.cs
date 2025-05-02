ef4fc73d6ef8d0c1909bee02e543beba5f0df65f
bai231 <chaiyibai6925@gmail.com>
2025-05-02 23:38:01 +0800

    select toolbar slots by mouse clicking and add the highlight

--- final project/Assets/scripts/UI/Slot_UI.cs
+++ final project/Assets/scripts/UI/Slot_UI.cs
@@ -7,6 +7,8 @@ public class Slot_UI : MonoBehaviour
     public Image itemIcon;
     public TextMeshProUGUI quantityText;
 
+    [SerializeField] private GameObject highlight;
+
     public void SetItem(Inventory.Slot slot)
     {
         if(slot != null)
@@ -22,5 +24,11 @@ public class Slot_UI : MonoBehaviour
         itemIcon.sprite = null;
         itemIcon.color = new Color(1, 1, 1, 0);
         quantityText.text = "";
+    }
+
+    public void SetHighlight(bool isOn)
+    {
+        highlight.SetActive(isOn);
+
     }
 }
