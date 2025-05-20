de1896b942c7244f0b54dca74352b223be6dbe8b
tiremouse <paru0585@gmail.com>
2025-05-21 01:57:36 +0800

    這次真的修好golem了

--- final project/Assets/scripts/enemy/golem/face_to_player.cs
+++ final project/Assets/scripts/enemy/golem/face_to_player.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public class face_to_player : MonoBehaviour
 {
+    public Transform player;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public Transform player; 
     void Start()
     {
         
@@ -12,10 +12,10 @@ public class face_to_player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       // 5. 面向玩家
+        // 5. 面向玩家
         Vector3 scale = transform.localScale;
         float originalX = Mathf.Abs(scale.x); // 保留原本的大小
         scale.x = player.position.x < transform.position.x ? -originalX : originalX;
-        transform.localScale = scale; 
+        transform.localScale = scale;
     }
 }
