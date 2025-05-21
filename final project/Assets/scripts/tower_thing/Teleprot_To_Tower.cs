480e0fc471003497cc62d489fe8deef21133b6bf
uhumipa <dragonmonth666@gmail.com>
2025-05-21 16:02:25 +0800

    做了進入塔的UI

--- final project/Assets/scripts/tower_thing/Teleprot_To_Tower.cs
+++ final project/Assets/scripts/tower_thing/Teleprot_To_Tower.cs
@@ -4,15 +4,23 @@ using UnityEngine.SceneManagement;
 
 public class Teleprot_To_Tower : MonoBehaviour
 {
-    private Maploaders loader;
+    public GameObject towerEnterPanel;
+    public GameObject floorChoicePanel;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] bool playerInRange = false;
 
+
+    void Start()
+    {
+        towerEnterPanel.SetActive(false);
+        floorChoicePanel.SetActive(false);
+        
+    }
     void Update()
     {
         if (playerInRange && Input.GetKeyDown(KeyCode.Space)) // Enter鍵
         {
-            SceneManager.LoadScene("tower");  
+            OpenEnterPanel();
         }
     }
 
@@ -30,6 +38,43 @@ public class Teleprot_To_Tower : MonoBehaviour
         {
             playerInRange = false;
         }
+    }
+
+
+    public void OpenEnterPanel()
+    {
+        towerEnterPanel.SetActive(true);
+    }
+
+    public void OnClickEnterYes()
+    {
+        towerEnterPanel.SetActive(false);
+        floorChoicePanel.SetActive(true);
+    }
+
+    public void OnClickEnterNo()
+    {
+        towerEnterPanel.SetActive(false);
+    }
+
+    public void OnClickStartFromBeginning()
+    {
+        TowerManager.Instance.currentTowerFloor= 1;
+        SceneManager.LoadScene("TowerScene");
+    }
+
+    public void OnClickStartFromMiddle()
+    {
+        for (int i = 0; i <10; i++)
+        {
+            if (TowerManager.Instance.currentTowerFloor % 10 == 5)
+            {
+                break;
+            }
+            TowerManager.Instance.currentTowerFloor--;
+        }
+            // 比如從第 11 層開始
+            SceneManager.LoadScene("TowerScene");
     }
 
 }
