bf91a15072bf6a3329b2b84658bf1b6d177928a4
bai231 <chaiyibai6925@gmail.com>
2025-05-22 17:03:20 +0800

    加了一點NPC動畫

---
+++ final project/Assets/scripts/NPC/NPC_Patrol.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class NPC_Patrol : MonoBehaviour
+{
+    public Vector2[] patrolPoints;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
