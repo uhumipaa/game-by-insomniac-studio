44d272e61b6ec9fb603235dfae9f5a39e1552224
uhumipa <dragonmonth666@gmail.com>
2025-05-28 01:30:25 +0800

    修正切換場景tilemap抓不到的bug

--- final project/Assets/scripts/tile/TileManager.cs
+++ final project/Assets/scripts/tile/TileManager.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.Tilemaps;
+using UnityEngine.SceneManagement;
 
 
 public class TileManager : MonoBehaviour
@@ -14,8 +15,21 @@ public class TileManager : MonoBehaviour
     public TileBase matureTile; //成長
     public TileBase readyToHarvestTile; //準備收成
 
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "farm")
+        {
+            var tilemapObj = GameObject.Find("CropTilemap");
+            if (tilemapObj != null)
+                cropTilemap = tilemapObj.GetComponent<Tilemap>();
+            var Interactobj = GameObject.Find("InteractableMap");
+            if (Interactobj != null)
+                InteractableMap = Interactobj.GetComponent<Tilemap>();
+        }
+    }
     void Start()
     {
+        SceneManager.sceneLoaded += OnSceneLoaded;
         foreach (var position in InteractableMap.cellBounds.allPositionsWithin)
         {
             InteractableMap.SetTile(position, hiddenInteractableTile);
