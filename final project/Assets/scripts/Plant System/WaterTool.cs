c86c504e3f694e7804ede1fc52bd9feef5c0d122
bai231 <chaiyibai6925@gmail.com>
2025-05-31 23:21:47 +0800

    新增澆水跟修一些bug

---
+++ final project/Assets/scripts/Plant System/WaterTool.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class WaterTool : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1)) // 右鍵澆水
+        {
+            Debug.Log("點擊澆水");
+            Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3Int cellPos = FarmManager.instance.tileManager.cropTilemap.WorldToCell(mouseWorldPos);
+
+            var selectedSlot = InventoryManager.Instance.toolbar.selectedSlot;
+            if (TryWater(cellPos, selectedSlot))
+            {
+                
+                Debug.Log("澆水成功");
+            }
+        }
+    }
+    
+
+    public bool TryWater(Vector3Int position, Inventory.Slot selectedSlot)
+    {
+        Debug.Log($"🎯 當前物品: {selectedSlot.itemData.itemName}, 類型: {selectedSlot.itemData.type}, 數量: {selectedSlot.count}");
+        // 檢查該格是否已經種田
+        if (!FarmManager.instance.HasFarmTile(position)) return false;
+
+        var tileData = FarmManager.instance.GetFarmTileData(position);
+
+        // 檢查是否已經澆水
+        if (tileData.isWatered) return false;
+
+        // 檢查是否選到物品
+        if (selectedSlot == null || string.IsNullOrEmpty(selectedSlot.itemData.itemName)) return false;
+        if (selectedSlot.count <= 0) return false;
+
+        // 檢查是否為水
+        if (selectedSlot.itemData.type != ItemType.Water) return false;
+
+        // 扣除水的數量
+        selectedSlot.count--;
+        //UI_Manager.Instance.RefreshInventoryUI("Toolbar");
+
+
+        // 執行澆水
+        FarmManager.instance.WaterTile(position);
+
+        return true;
+    }
+}
